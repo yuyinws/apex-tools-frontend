85a6c27ac3bbe3954ad3515a237c3172c05c786e
yuyinws <lyc657508366@gmail.com>
2021-12-24 17:08:13 +0800

    feat:PWA support

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,58 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 import styleImport from 'vite-plugin-style-import'
-// https://vitejs.dev/config/
+
+const pwaOptions = {
+  manifest: {
+    mode: 'development',
+    base: '/',
+    includeAssets: ['favicon.ico'],
+    name: 'Apex Tools',
+    short_name: 'Apex Tools',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+  ;(pwaOptions.manifest).name = 'Apex Tools'
+  ;(pwaOptions.manifest).short_name = 'Apex Tools'
+}
+
+if (claims)
+  pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-ignore
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+
+
 export default defineConfig({
   plugins: [
     vue(),
